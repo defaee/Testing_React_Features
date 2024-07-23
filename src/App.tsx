e7a7792f77de9f3fa343536d00b1638db7abf653
Pooya Defaee <pooya.defaee@gmail.com>
2024-07-23 18:30:18 +0330

    make a react + TS app and add tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="bg-black w-full h-screen flex justify-center items-center">
+      <h1 className="text-2xl text-white">Hello World!</h1>
+    </main>
+  );
+}
+
+export default App;
