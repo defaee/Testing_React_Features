c9f997918ee73062b4f999776edd732215db7413
Pooya Defaee <pooya.defaee@gmail.com>
2024-07-25 13:54:02 +0330

    add search option

--- src/components/books.tsx
+++ src/components/books.tsx
@@ -1,5 +1,6 @@
-import { Link, Outlet, useParams } from "react-router-dom";
+import { Link, Outlet, useParams, useSearchParams } from "react-router-dom";
 import data from "../assets/data.json";
+import { useEffect, useState } from "react";
 
 interface Book {
   id: number;
@@ -8,12 +9,35 @@ interface Book {
 }
 
 const Books = () => {
+  const [text, setText] = useState<string>("");
+  const [serachParams, setSearchParams] = useSearchParams();
+  const handleSearch = ({ value }: { value: string }) => {
+    setText(value);
+    if (!value.trim()) {
+      setSearchParams({});
+      return;
+    }
+    setSearchParams({ q: value.trim() });
+  };
   const { bookID } = useParams();
   const ID: number | undefined = Number(bookID);
+
+  useEffect(() => {
+    const query = serachParams.get("q") || "";
+    handleSearch({ value: query });
+  }, [serachParams]);
+
+  const filteredBooks = text ? data.books.filter((book: Book) => book.title.toLowerCase().includes(text.toLowerCase())) : data.books;
   return (
     <div className="w-[75%] flex justify-between items-center">
       <div className="w-[28%] flex flex-col py-[3rem] gap-[1rem]">
-        {data.books.map((book: Book) => (
+        <input
+          placeholder="Search a Book"
+          className="w-[50%] px-3 py-1 rounded-lg"
+          value={text}
+          onChange={({ target }) => handleSearch(target)}
+        />
+        {filteredBooks.map((book: Book) => (
           <Link className={`${book.id === ID ? "text-red-500" : ""}`} to={`/books/${book.id}`}>
             {book.title}
           </Link>
